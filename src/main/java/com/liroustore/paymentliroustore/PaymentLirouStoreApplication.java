65247806b51a3d59d6671772e08d4c4fb4b5d438
Patrick Melo <patrickabimael@yahoo.com.br>
2024-01-28 11:20:37 -0300

    primeiro commit

---
+++ src/main/java/com/liroustore/paymentliroustore/PaymentLirouStoreApplication.java
@@ -0,0 +1,13 @@
+package com.liroustore.paymentliroustore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentLirouStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentLirouStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liroustore/paymentliroustore/PaymentLirouStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liroustore.paymentliroustore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentLirouStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
