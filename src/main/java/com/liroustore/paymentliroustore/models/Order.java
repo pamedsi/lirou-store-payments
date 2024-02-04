ac6801659ee6bd676a330c6aa4257538f001e56f
Patrick Melo <patrickabimael@yahoo.com.br>
2024-02-04 09:23:19 -0300

    valida campos obrigatórios

--- src/main/java/com/liroustore/paymentliroustore/models/Order.java
+++ src/main/java/com/liroustore/paymentliroustore/models/Order.java
@@ -1,25 +1,28 @@
 package com.liroustore.paymentliroustore.models;
 
-
 import com.liroustore.paymentliroustore.models.shared.Address;
+import com.liroustore.paymentliroustore.types.PhoneType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record Order(
-        String referenceId,
-        Customer customer, List<Item> items,
-        List<QRCode> qrCodes,
+        String reference_id,
+        @Valid Customer customer,
+        List<Item> items,
         Shipping shipping,
-        List<String> notificationUrls
+        List<QRCode> qr_codes,
+        List<String> notification_urls
 ){}
 
-record Customer(String name, String email, String taxId, List<Phone> phones) {}
+record Customer(String name, String email, @NotBlank String tax_id, List<Phone> phones) {}
 
-record Phone(String country, String area, String number, String type) {}
+record Phone(int country, int area, int number, PhoneType type) {}
 
 record Item(String name, int quantity, double unitAmount) {}
 
-record QRCode(Amount amount, LocalDateTime expirationDate) {}
+record QRCode(Amount amount, LocalDateTime expiration_date) {}
 
 record Shipping(Address address) {}
