9634ef6712db649cb7166bf180d2d955de3548d0
Patrick Melo <patrickabimael@yahoo.com.br>
2024-02-05 08:03:22 -0300

    comenta controlador de notificação

--- src/main/java/com/liroustore/paymentliroustore/controller/PagBankController.java
+++ src/main/java/com/liroustore/paymentliroustore/controller/PagBankController.java
@@ -1,6 +1,6 @@
 package com.liroustore.paymentliroustore.controller;
 
-import com.liroustore.paymentliroustore.models.PaymentTransaction;
+//import com.liroustore.paymentliroustore.models.PaymentTransaction;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 public class PagBankController {
 
-    @PostMapping("/notify")
-    public ResponseEntity<?> onStatusChange(@RequestBody PaymentTransaction paymentTransaction) {
-        return ResponseEntity.ok("notificando!");
-    }
+//    @PostMapping("/notify")
+//    public ResponseEntity<?> onStatusChange(@RequestBody PaymentTransaction paymentTransaction) {
+//        return ResponseEntity.ok("notificando!");
+//    }
 
     @GetMapping
     public ResponseEntity<String> testar(){
